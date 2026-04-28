8ede37243374a9504cd0fd3bc5c72ef436fee2ed
Christine Thornton <237637779+finallychristine@users.noreply.github.com>
2026-04-28 10:29:20 -0700

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,35 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import https from 'node:https';
+
+const wkdUrl = /\/\.well-known\/openpgpkey\/([a-zA-Z0-9.]+)\/policy/
+const protonHostname = "https://api.protonmail.ch"
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		if (request.method !== 'GET') {
+			return new Response('Method not allowed', { status: 405 });
+		}
+
+		const url = URL.parse(request.url);
+		if (url == null) {
+			throw new Error('null URL');
+		}
+
+		const path = url.pathname
+
+		if (!path.match(wkdUrl)) {
+			return new Response('URL should match: /.well-known/openpgpkey/example.com/policy', { status: 400 });
+		}
+
+
+		const proxyResp = await fetch(`https://${protonHostname}${path}`, {
+			method: 'GET',
+		})
+
+		console.log(proxyResp.status)
+
 		return new Response("Hello World!");
 	},
 } satisfies ExportedHandler<Env>;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,29 +1,25 @@
+import { env } from "cloudflare:workers";
 import {
-	env,
 	createExecutionContext,
 	waitOnExecutionContext,
-	SELF,
 } from "cloudflare:test";
 import { describe, it, expect } from "vitest";
-import worker from "../src/index";
+// Import your worker so you can unit test it
+import worker from "../src";
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+const IncomingRequest = Request;
 
-describe("Hello World worker", () => {
-	it("responds with Hello World! (unit style)", async () => {
-		const request = new IncomingRequest("http://example.com");
-		// Create an empty context to pass to `worker.fetch()`.
+describe("worker", () => {
+	it("forwards the policy url to proton", async () => {
+		const request = new IncomingRequest("https://example.com/.well-known/openpgpkey/christine.fyi/policy");
+		// Create an empty context to pass to `worker.fetch()`
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
-
-	it("responds with Hello World! (integration style)", async () => {
-		const response = await SELF.fetch("https://example.com");
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(response.status).toBe(404);
+		expect(await response.text()).toBe("Not found");
 	});
 });
